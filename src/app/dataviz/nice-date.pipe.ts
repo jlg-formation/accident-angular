9b29ee061be85c1e039d5e2ca9be14b0ba406aa9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-26 11:56:37 +0200

    done !

--- src/app/dataviz/nice-date.pipe.ts
+++ src/app/dataviz/nice-date.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+moment.locale('fr');
 
 @Pipe({
   name: 'niceDate'
 })
 export class NiceDatePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: string): any {
+    // 2019-06-09T00:00:00Z
+    const date = moment(value.substr(0, 10), 'YYYY-MM-DD');
+    return date.format('ddd, D MMM YYYY');
   }
 
 }
